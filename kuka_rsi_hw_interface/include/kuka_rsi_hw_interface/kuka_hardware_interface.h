0cec50d85e9fd043752d4ccf981187d0c42a7cef
Restyled.io <commits@restyled.io>
2023-12-31 15:17:53 +0000

    Restyled by astyle

--- kuka_rsi_hw_interface/include/kuka_rsi_hw_interface/kuka_hardware_interface.h
+++ kuka_rsi_hw_interface/include/kuka_rsi_hw_interface/kuka_hardware_interface.h
@@ -77,55 +77,55 @@ class KukaHardwareInterface : public hardware_interface::RobotHW
 
 private:
 
-  // ROS node handle
-  ros::NodeHandle nh_;
+    // ROS node handle
+    ros::NodeHandle nh_;
 
-  unsigned int n_dof_;
+    unsigned int n_dof_;
 
-  std::vector<std::string> joint_names_;
+    std::vector<std::string> joint_names_;
 
-  std::vector<double> joint_position_;
-  std::vector<double> joint_velocity_;
-  std::vector<double> joint_effort_;
-  std::vector<double> joint_position_command_;
-  std::vector<double> joint_velocity_command_;
-  std::vector<double> joint_effort_command_;
+    std::vector<double> joint_position_;
+    std::vector<double> joint_velocity_;
+    std::vector<double> joint_effort_;
+    std::vector<double> joint_position_command_;
+    std::vector<double> joint_velocity_command_;
+    std::vector<double> joint_effort_command_;
 
-  // RSI
-  RSIState rsi_state_;
-  RSICommand rsi_command_;
-  std::vector<double> rsi_initial_joint_positions_;
-  std::vector<double> rsi_joint_position_corrections_;
-  unsigned long long ipoc_;
+    // RSI
+    RSIState rsi_state_;
+    RSICommand rsi_command_;
+    std::vector<double> rsi_initial_joint_positions_;
+    std::vector<double> rsi_joint_position_corrections_;
+    unsigned long long ipoc_;
 
-  std::unique_ptr<realtime_tools::RealtimePublisher<std_msgs::String> > rt_rsi_pub_;
+    std::unique_ptr<realtime_tools::RealtimePublisher<std_msgs::String> > rt_rsi_pub_;
 
-  std::unique_ptr<UDPServer> server_;
-  std::string local_host_;
-  int local_port_;
-  std::string remote_host_;
-  std::string remote_port_;
-  std::string in_buffer_;
-  std::string out_buffer_;
+    std::unique_ptr<UDPServer> server_;
+    std::string local_host_;
+    int local_port_;
+    std::string remote_host_;
+    std::string remote_port_;
+    std::string in_buffer_;
+    std::string out_buffer_;
 
-  // Timing
-  ros::Duration control_period_;
-  ros::Duration elapsed_time_;
-  double loop_hz_;
+    // Timing
+    ros::Duration control_period_;
+    ros::Duration elapsed_time_;
+    double loop_hz_;
 
-  // Interfaces
-  hardware_interface::JointStateInterface joint_state_interface_;
-  hardware_interface::PositionJointInterface position_joint_interface_;
+    // Interfaces
+    hardware_interface::JointStateInterface joint_state_interface_;
+    hardware_interface::PositionJointInterface position_joint_interface_;
 
 public:
 
-  KukaHardwareInterface();
-  ~KukaHardwareInterface();
+    KukaHardwareInterface();
+    ~KukaHardwareInterface();
 
-  void start();
-  void configure();
-  bool read(const ros::Time time, const ros::Duration period);
-  bool write(const ros::Time time, const ros::Duration period);
+    void start();
+    void configure();
+    bool read(const ros::Time time, const ros::Duration period);
+    bool write(const ros::Time time, const ros::Duration period);
 
 };
 
